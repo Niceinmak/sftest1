c57fb1c20baf119732dd98116cb5b295dba8407b
Glitch (phrygian-hospitable-plough) <none>
2021-12-31 13:07:04 +0000

    🎋🍽 Checkpoint ./commands/cf1.js:5606966/315

--- commands/cf1.js
+++ commands/cf1.js
@@ -81,12 +81,14 @@ exports.execute = async (client, message, args) => {
                      totalcash2=amount
                    }
                };
-     if(amount>11)
+            if(amount>11)
                {
                  amount=10
-                                      if(amount2<10)
+                   if(amount2<10)
                    {
                  embed.addField(`**Kasa [${amount}+?]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${amount2},${amount5}`)
+                   totalcash=amount2+amount5
+                     totalcash2=amount
                    }
                  if(amount2==11)
                    {
@@ -94,11 +96,15 @@ exports.execute = async (client, message, args) => {
                      if(amount4==0) amount2=10;
                      if(amount4==0) amount2=1;
                  embed.addField(`**Kasa [${amount}+?]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${acards},${amount5}`)
+                totalcash=amount2+amount5
+                     totalcash2=amount
                    }
                  if(amount2>11)
                    {
                      amount2=10
                  embed.addField(`**Kasa [${amount}+?]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${namescards[Math.floor(Math.random() * namescards.length)]},${amount5} `)
+                 totalcash=amount2+amount5
+                     totalcash2=amount
                    }
                   };
     
@@ -132,10 +138,10 @@ collector.on("collect", (reaction, user) => {
         {
             if(amount4>10 && amount4<14)
     {
-      embed.setField(`**Kasa [${amount+caseamount}]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${namescards[Math.floor(Math.random() * namescards.length)]} `)
+      embed.setField(`**Kasa [${totalcash2+caseamount}]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${namescards[Math.floor(Math.random() * namescards.length)]} `)
     }
           
-          embed.setField(`**Kasa [${amount+amount4}]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${namescards[Math.floor(Math.random() * namescards.length)]} `)
+          embed.setField(`**Kasa [${totalcash2+amount4}]**        ${user1.username} [${amount2+amount5}]`,`** Kasa ${namescards[Math.floor(Math.random() * namescards.length)]}  **       ${user1.username} ${namescards[Math.floor(Math.random() * namescards.length)]} `)
         }
     }
   return msg.edit(embed);
