deb0b955bf295fb992d9c57ccf21b3f89b0e7658
Glitch (phrygian-hospitable-plough) <none>
2021-12-31 19:04:21 +0000

    💡🚪 Checkpoint ./commands/slots.js:5606966/22223

--- commands/slots.js
+++ commands/slots.js
@@ -1,49 +1,46 @@
+
+const { MessageEmbed } = require("discord.js");
 exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 200)+50;
     let amount2 = Math.floor(Math.random() * 2);
     let amount4 = Math.floor(Math.random() * 200)/100;
     let amount3 = args[0]
-    let yazitura= ""
     let authordata = client.eco.fetchMoney(message.author.id) 
-        let timecooldown = Math.floor(Math.random() * 200)+50;
-        let playtime = await client.eco.beg(client.ecoAddUser, timecooldown,{cooldown: 5000});
-  let data2= client.eco.removeMoney(message.author.id, parseInt(timecooldown));
+    let timecooldown = Math.floor(Math.random() * 200)+50;
+    let playtime = await client.eco.beg(client.ecoAddUser, timecooldown,{cooldown: 5000});
+    let data2= client.eco.removeMoney(message.author.id, parseInt(timecooldown));
+    const user1 = message.mentions.users.first() || message.member.user
     if (playtime.onCooldown) return message.reply(`**Biraz yavaş ol,${playtime.time.seconds} saniye daha bekle.**`);
-
-  else{
   //--------------------------------------------
     if (!amount3 || isNaN(amount3)) return message.channel.send(`** ⛔${message.author.tag} | ** Lütfen Sayı Giriniz`);
+  else{
     if(amount3>authordata.amount || amount3<1)  return message.channel.send(`** ⛔${message.author.tag} | ** Girdiğiniz miktar paranızdan fazla veya 1'den az olamaz`);
     else
     {
-       if([Math.floor(amount4)]==0) yazitura="Tura";
-          else yazitura="Yazı";
-                message.channel.send(`**${message.author.tag} | ** **${amount3}**💶 oynadı ve **${yazitura}** seçti! \n Para döndürülüyor.🪙`).then(async msg => {
-        setTimeout(() => {
-          msg.edit(`**${message.author.tag} | ** **${amount3}**💶 oynadı ve **${yazitura}** seçti! \n Para döndürülüyor..🪙`);
-        }, 1000);
+      
+             const embed = new MessageEmbed()
+             .setAuthor(`${user1.username}, ${amount3} ile blackjack oynadı`, user1.displayAvatarURL())
+            .setFooter(`Oyun devam ediyor`)
+            .setTitle(`${user1.username}`)
+            .addField("API Gecikmesi", `s`)
+            .addField("Client Gecikmesi", `ms`)
+            .setColor("#7289DA")
+            .setTimestamp();
+              return message.channel.send(embed).then(async msg => {
                 setTimeout(() => {
-          msg.edit(`**${message.author.tag} | ** **${amount3}**💶 oynadı ve **${yazitura}** seçti! \n Para döndürülüyor...🪙`);
-        }, 2000);
-                          setTimeout(() => {
-           if([Math.floor(amount2)]==0)
-        {
-            let data = client.eco.addMoney(message.author.id, parseInt(amount3));
-      msg.edit(`**${message.author.tag} | ** **${amount3}**💶 oynadı ve **${yazitura}** seçti! \n Para döndürülüyor...🪙 ve **${(amount3*2)}**💶 Kazandın!`);
-        }
-    else {
-      let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
-     msg.edit(`**${message.author.tag} | ** **${amount3}**💶 oynadı ve **${yazitura}** seçti! \n Para döndürülüyor...🪙 ve maalesef **kaybettin :c**`);
+   embed.setAuthor(`as`)
+          return msg.edit(embed);
+                   }, 1000);
+                setTimeout(() => {
+         embed.setAuthor(`sa`)
+          return msg.edit(embed);
+              }, 1000);
+        } );
     }
-        }, 3000);
-        })
-     
-    }
-    
-    }};
+    }  };
 
 exports.help = {
-    name: "cf",
+    name: "slots",
     aliases: ["coinflip","yazıtura"],
-    usage: "slots <yazı,tura> <miktar>"
+    usage: "slots <miktar>"
 }
