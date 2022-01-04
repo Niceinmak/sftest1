496ef348d43610a3572a3e47b6561293f543fcbc
Glitch (phrygian-hospitable-plough) <none>
2022-01-04 19:41:40 +0000

    🚣🚈 Checkpoint ./commands/uptime.js:5606966/362

--- commands/uptime.js
+++ commands/uptime.js
@@ -8,7 +8,7 @@ let hours = Math.floor(totalSeconds / 3600);
 totalSeconds %= 3600;
 let minutes = Math.floor(totalSeconds / 60);
 let seconds = Math.floor(totalSeconds % 60);
-    return message.channel.send(`** ⛔${message.author.tag} | Bot Running Time \`${days} hour ${hours} hours ${minutes} minutes  ${seconds} seconds**`);
+    return message.channel.send(`**${message.author.tag} | Bot Running Time \`${days}\` hour \`${hours}\` hours \`${minutes}\` minutes  \`${seconds}\` seconds**`);
 }
 
 exports.help = {
