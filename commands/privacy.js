5943e255561d25aad7ee7985f592dc740359e611
Glitch (phrygian-hospitable-plough) <none>
2022-01-07 19:43:41 +0000

    🐷🛬 Checkpoint ./commands/privacy.js:5606966/2860

--- commands/privacy.js
+++ commands/privacy.js
@@ -1,6 +1,13 @@
 const { MessageEmbed } = require("discord.js");
 const { MessageActionRow, MessageButton } = require('discord.js');
 exports.execute = async (client, message, args) => {
+   	const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setCustomId('primary')
+					.setLabel('Primary')
+					.setStyle('PRIMARY'),
+			);
     const embed = new MessageEmbed()
         .setAuthor("Privacy Policy")
         .setTitle("EcoVerse Privacy Policy")
@@ -29,39 +36,7 @@ If you wish to delete your data, please contact us: https://discord.gg/2n9Zg9BGg
         .setTimestamp()
         .setThumbnail(client.user.displayAvatarURL)
         .setFooter(message.author.tag, message.author.displayAvatarURL);
-  const row = new MessageActionRow()
-			.addComponents(
-				new MessageButton()
-					.setCustomId('primary')
-					.setLabel('I agree')
-					.setStyle('PRIMARY'),
-			);
-   await message.channel.send({ephemeral: true, embeds: [embed], components: [row] });
-  /*
-    FARKLI KOMUTLAR DİZİNİ
-
-
-exports.execute = async (client, message, args) => {
-    let users = [
-        "Pocket",
-        "T-Shirt",
-        "Zero's Databse",
-        "Street"
-    ];
-    let amount = Math.floor(Math.random() * 200)+50;
-    let amount2 = Math.floor(Math.random() * 200)/100;
-    let amount3 = args[0]
-    let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
-    let beg2 = await client.eco.beg(client.ecoAddUser, amount2, { canLose: false, cooldown: 1, customName: "search" });
-    if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
-    return message.reply(`**${message.author.tag} | ** **${[Math.floor(beg2.amount)]}** Paranı **2'ye** **${amount}** katladın ve **${amount3}** 💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.`);
-       let data2= client.eco.removeMoney(message.author.id, parseInt(kullaniciveri));
-       let data2= client.eco.removeMoney(client.ecoAddUser, parseInt(kullaniciveri));    
-       let data2= client.eco.addMoney(message.author.id, parseInt(kullaniciveri));
-       let data2= client.eco.addMoney(client.ecoAddUser, parseInt(kullaniciveri));        
-       let data2= client.eco.setMoney(message.author.id, parseInt(kullaniciveri));
-        let data2= client.eco.setMoney(client.ecoAddUser, parseInt(kullaniciveri));           
----------------------------------------------------------------------------------------*/
+    return message.channel.send(embed,row);
 }
 
 exports.help = {
