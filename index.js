3bef3e5513830419ed89536fffac35fcfcb1ed8a
Glitch (phrygian-hospitable-plough) <none>
2022-01-08 06:41:25 +0000

    🐵🍎 Checkpoint ./index.js:5606966/2688 ./package.json:5606966/1088

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const Discord = require("discord.js");
  const client = new Discord.Client({ disableMentions: 'everyone' });
 const Eco = require("quick.eco");
+const disbut = require("discord-buttons");
 client.eco = new Eco.Manager(); // quick.eco
 client.db = Eco.db; // quick.db
 client.config = require("./botConfig");
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 client.shop = {
-  "yaygın.kasa" : {
+  "common.case" : {
     cost: 100
   },
-  "nadir.kasa" : {
+  "rare.case" : {
     cost: 1000
   },
-  "epik.kasa" : {
+  "epic.case" : {
     cost: 10000
   }
 };
