615cd8314b5d9a6058d47def1b3e3d5950040883
Glitch (phrygian-hospitable-plough) <none>
2022-01-08 08:26:03 +0000

    💽👔 Checkpoint ./commands/privacy.js:5606966/1670

--- commands/privacy.js
+++ commands/privacy.js
@@ -38,6 +38,25 @@ If you wish to delete your data, please contact us: https://discord.gg/2n9Zg9BGg
 	button: button,
   	embed: embed
 })
+ message.channel.send({ buttons: [button], embed: embed }).then(message => { // Send Embed And Buttons
+                const filter = (button) => button.clicker.user.id === member.id // To Check If User Who Clicked Button Is Same As Who Used Command
+                const collector = message.createButtonCollector(filter, { time: 30000 }) // 30 Seconds To Click
+
+                collector.on('collect', async b => {
+                    b.defer()
+                    if(b.id === 'yes') { // If User Click Yes Button
+                        member.setNickname(name)
+                        b.channel.send(`Successfully Chnaged NickName To **${name}**`)
+                        db.set(`name-time_${message.guild.id}_${member.id}`, Date.now())
+                        collector.stop()
+                    } else if(b.id === 'no') { // If User Click No Button
+                        b.channel.send(`Request Cancelled`)
+                        db.set(`name-time_${message.guild.id}_${member.id}`, Date.now())
+                        collector.stop()
+                    }
+                })
+
+            })
   /*
     FARKLI KOMUTLAR DİZİNİ
 
