a8aba306b02a4fec6e40c062398b5339d87fd54b
Glitch (phrygian-hospitable-plough) <none>
2022-01-08 12:45:38 +0000

    🏇🦂 Checkpoint ./package.json:5606966/2255 ./index.js:5606966/2664 ./events/ready.js:5606966/2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require("discord.js");
-const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const Eco = require("quick.eco");
+ const client = new Discord.Client({ disableMentions: 'everyone' });
 const disbut = require('discord-buttons');
 disbut(client);
+const Eco = require("quick.eco");
 client.eco = new Eco.Manager(); // quick.eco
 client.db = Eco.db; // quick.db
 client.config = require("./botConfig");
