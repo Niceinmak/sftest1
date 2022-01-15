7fc3a78e0ee58c62b1c358e090a74989e54dc673
Glitch (phrygian-hospitable-plough) <none>
2022-01-15 10:31:18 +0000

    👜🍏 Checkpoint ./commands/bal.js:5606966/1471 ./commands/transfer.js:5606966/2011

--- commands/transfer.js
+++ commands/transfer.js
@@ -1,16 +1,17 @@
 exports.execute = async (client, message, args) => {
   let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) 
   let authordata = client.eco.fetchMoney(message.author.id) 
-  if (!member) return message.channel.send('Kime para göndermek istiyorsun?') 
+  if (!member) return message.channel.send('Please mention the person or give their ID') 
   let amount = args[1]
-  if (!amount || isNaN(amount)) return message.channel.send('Lütfen geçerli bir miktar giriniz') 
-  if(authordata.amount < amount) return message.channel.send('Görünüşe göre senin o kadar paran yok') 
+  if (!amount || isNaN(amount)) return message.channel.send('Please enter a valid amount to transfer') 
+  if(authordata.amount < amount) return message.channel.send('Looks like you don\'t have that much money') 
   if(message.author.id==member.id) return message.channel.send("You **can't** send money to yourself :D")
   await client.eco.transfer(message.author.id, member.id, amount) 
-  return message.channel.send(`Gönderme işlemi başarılı **${amount}**💶 parayı ** ${member.user.tag}** kişisine gönderdin.`)
+  let amountformat=String(amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+  return message.channel.send(`You have successfully transferred 💶**${amountformat}** to ** ${member.user.tag}**.`)
 }
 exports.help = {
   name: "transfer",
-  aliases: ['give', 'share', 'send'],
+  aliases: ['give', 'share', 'send',"SEND","TRANSFER"],
   usage: `transfer <member> <amount>`
 };
