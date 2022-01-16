9a1eefa425b4c3f554195b8652c0330619dd77d5
Glitch (phrygian-hospitable-plough) <none>
2022-01-16 13:36:37 +0000

    🌹🏕 Checkpoint ./commands/roll.js:5606966/310

--- commands/roll.js
+++ commands/roll.js
@@ -22,40 +22,41 @@ exports.execute = async (client, message, args) => {
     if (playtime.onCooldown) return message.reply(`**Take it slow,wait ${playtime.time.seconds} more seconds**`);
    let data2= client.eco.removeMoney(message.author.id, parseInt(timecooldown));
   //--------------------------------------------
-  if(amount3=="all") amount3=authordata.amount;
-  if(amount3=="half") amount3=authordata.amount/2 , amount3=parseInt(amount3);
-  if(amount3>50000) amount3=50000;
-    if (!amount3 || isNaN(amount3)) return message.channel.send(`** ⛔${message.author.tag} | ** Please specify a valid amount.`);
+  if(amount4=="all") amount3=authordata.amount;
+  if(amount4=="half") amount3=authordata.amount/2 , amount3=parseInt(amount3);
+  if(amount4>50000) amount4=50000;
+    if (!amount4 || isNaN(amount4)) return message.channel.send(`** ⛔${message.author.tag} | ** Please specify a valid amount.`);
+  if (!amount3 || isNaN(amount3) || amount3>6 || amount<0) return message.channel.send(`** ⛔${message.author.tag} | ** Please select 1,2,3,4,5,6`);
   else{
-    if(amount3>authordata.amount || amount3<1)  return message.channel.send(`** ⛔${message.author.tag} | ** You don't have enough money`);
+    if(amount4>authordata.amount || amount4<1)  return message.channel.send(`** ⛔${message.author.tag} | ** You don't have enough money`);
     else
     {
-               message.channel.send(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**`).then(async msg => {
+               message.channel.send(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**`).then(async msg => {
                  let randomnumber=numbers[Math.floor(Math.random() * numbers.length)]
                 setTimeout(() => {
   // embed.setAuthor(`as`)
-          msg.edit(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**\`.\``)
+          msg.edit(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**\`.\``)
                    }, 500);
                 setTimeout(() => {
        //  embed.setAuthor(`sa`)
-          msg.edit(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**\`..\``)
+          msg.edit(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**\`..\``)
               }, 1000);
             setTimeout(() => {
-             msg.edit(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**\`...\``)
+             msg.edit(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**\`...\``)
               }, 2000);
           setTimeout(() => {
-            msg.edit(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**\`${randomnumber}\``)
+            msg.edit(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**\`${randomnumber}\``)
               }, 3000);
            setTimeout(() => {
              if(amount3==randomnumber)
                 {
-                  msg.edit(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**\`${randomnumber}\`\n\`Congrulations,You Win ${amount4*3}💶!\``)
+                  msg.edit(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**\`${randomnumber}\`\n\`Congrulations,You Win ${amount4*3}💶!\``)
                   let data = client.eco.addMoney(message.author.id, parseInt(amount4*3));
                 }
              else
                {
                  let data = client.eco.removeMoney(message.author.id, parseInt(amount4));
-                 msg.edit(`**Niceinmak *played* roll:** \`${amount3}\`\n**Number on the dice:**\`${randomnumber}\`\n\`Sorry,You Lost ${amount4}💶\``)
+                 msg.edit(`**🎲 | Niceinmak *played* roll:** \`${amount3}\`\n**🎲 | Number on the dice:**\`${randomnumber}\`\n\`Sorry,You Lost ${amount4}💶\``)
                }
               }, 4000);
         } );
