38f7352adc99bf3aa72ee99a3715a4262aaf7e9d
Glitch (phrygian-hospitable-plough) <none>
2022-01-24 07:38:01 +0000

    🐔🎱 Checkpoint ./commands/daily2.js:5606966/2337

--- commands/daily2.js
+++ commands/daily2.js
@@ -1,14 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client()
+let db = require('quick.db');
 const DBL = require("dblapi.js");
+let ms = require('ms')
 const dbl = new DBL(process.env.TOPGG_TOKEN, + client);
-let db = require('quick.db')
 module.exports.execute = async (client, message, args) => {
-  const timeout = 604800000;
-  const cooldown = await db.fetch(`cooldown_Command-Name_${message.guild.id}_${message.author.id}`);
+  const timeout = 5000;
+  const cooldown = await db.fetch(`cooldown_Command-Name_${message.author.id}`);
     let amount = Math.floor(Math.random() * 2000) + 100;
-    let addMoney = client.eco.daily(message.author.id, amount,{cooldown: 5000});
-    if (addMoney.onCooldown) 
+  	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
+		const time = ms(timeout - (Date.now() - cooldown));
+		message.channel.send(`1 gün bekle`);
+	} else {
+      dbl.hasVoted(message.author.id).then(voted => {
+    if (voted){
+       message.reply(`Bu gün **${amount}** 💸topladın`);
+      db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
+    }
+    else if (!voted){
+       return message.reply(`oyla knk`);
+    }
+		
+}
+ /*  if (addMoney.onCooldown) 
       {
       return message.reply(`Bugünkü paranı topladın.Lütfen ${addMoney.time.hours} saat, ${addMoney.time.minutes} dakika & ${addMoney.time.seconds} saniye sonra tekrar gel.`);
       }
@@ -23,7 +37,7 @@ module.exports.execute = async (client, message, args) => {
     }
 })
     
-    }
+    } */
 };
 
 module.exports.help = {
