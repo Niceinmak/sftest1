f82f9256f7dd10f8e65829055c7ef2b68ff50295
Glitch (phrygian-hospitable-plough) <none>
2022-01-24 08:47:08 +0000

    🍢🛰 Checkpoint ./commands/daily2.js:5606966/6963

--- commands/daily2.js
+++ commands/daily2.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { MessageButton } = require('discord-buttons')
 const client = new Discord.Client()
 let db = require('quick.db');
 const DBL = require("dblapi.js");
@@ -7,11 +9,11 @@ const dbl = new DBL(process.env.TOPGG_TOKEN, + client);
 module.exports.execute = async (client, message, args) => {
   let user = message.mentions.users.first() || message.author;
     
-  const timeout = 86400000;
+  const timeout = 5000;
   const cooldown = await db.fetch(`cooldown_Command-Name_${message.author.id}`);
     let amount = Math.floor(Math.random() * 5000) + 300;
     let amountformat=String(amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
-      dbl.hasVoted(message.author.id).then(voted => {
+      dbl.hasVoted("message.author.id").then(voted => {
     if (voted){
         	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
 		const time = ms(timeout - (Date.now() - cooldown));
@@ -24,7 +26,20 @@ module.exports.execute = async (client, message, args) => {
       db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
     }
     else if (!voted){
-       return message.reply(`**Please rate the bot at \`https://top.gg/bot/924311092468015116/vote\`**`);
+         const embed = new MessageEmbed()
+        .setTitle("EcoVerse Vote")
+     //   .setURL("https://www.youtube.com/channel/UCF9E-xef9jL9QgziZRDHKKQ")
+        .setDescription(`**Please rate the bot at \`https://top.gg/bot/924311092468015116/vote\`**`)
+        .setColor("BLURPLE")
+        .setTimestamp()
+        .setThumbnail(client.user.displayAvatarURL)
+        .setFooter(message.author.tag, message.author.displayAvatarURL);
+        let buttonurl = new MessageButton()
+  .setStyle('url')
+    .setURL("https://top.gg/bot/924311092468015116/vote")
+  .setLabel('Vote') 
+  .setDisabled(false);
+       return message.channel.send({ buttons: [buttonurl], embed: embed});
     }
       
 		
