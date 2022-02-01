cabcf8a5a2eaebde9880f3119300866fe3baa14a
Glitch (phrygian-hospitable-plough) <none>
2022-02-01 10:49:26 +0000

    📼🎄 Checkpoint ./commands/userinfo.js:5606966/8497

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,23 +1,13 @@
-const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
-const client = new Discord.Client()
-let db = require('quick.db');
-const DBL = require("dblapi.js");
-const dbl = new DBL(process.env.TOPGG_TOKEN, + client);
 exports.execute = async (client, message, args) => {
   const user = message.mentions.users.first() || message.author;
   let userBalance = client.eco.fetchMoney(user.id);
   let userBalanceformat=String(userBalance.amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let items=0
+  let admin=``
+  if (!client.config.admins.includes(message.author.id)) admin=`No`
+  else admin=`Yes`
   let itemsname=``
-  let votedtext=`none`
-   dbl.hasVoted(message.author.id).then(voted => {
-    if (voted){
-       votedtext=`${user.username} vote available :ballot_box_with_check: `
-    }
-    else if (!voted){
-     votedtext=`**${user.username} has voted today**`
-    }})
    const x = client.db.get(`items_${user.id}`);
   if (!x) {
     items=0
@@ -44,7 +34,6 @@ const Embed1 = new MessageEmbed()
 	.addFields(
     { name: '**Money**', value: `**User: ${user.username}\nMoney: ${userBalanceformat}💶\nPosition: ${userBalance.position}**` },
 		{ name: '**Items**', value: `${itemsname}` },
-    { name: '**Items**', value: `${votedtext}` },
 		{ name: '\u200B', value: '\u200B' },
 		{ name: 'Inline field title', value: 'Some value here', inline: true },
 		{ name: 'Inline field title', value: 'Some value here', inline: true },
