98302557bb23fbbe52b08d14648b055c5330bd92
Glitch (phrygian-hospitable-plough) <none>
2022-02-01 09:38:00 +0000

    🚖👞 Checkpoint ./commands/userinfo.js:5606966/1655

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,10 +4,22 @@ exports.execute = async (client, message, args) => {
   const user = message.mentions.users.first() || message.author;
   let userBalance = client.eco.fetchMoney(user.id);
   let userBalanceformat=String(userBalance.amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+  let items=0
+   const x = client.db.get(`items_${user.id}`);
+  if (!x) {
+    items=0
+  }
+  const arrayToObject = x.reduce((itemsobj, x) => {
+    itemsobj[x.name] = (itemsobj[x.name] || 0) + 1;
+    return itemsobj;
+  }, {});
+  const result = Object.keys(arrayToObject).map(k =>
+    items++
+  );
 const Embed1 = new MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle(user.username)
-	.setDescription(`**Total Cash: ${userBalanceformat}\nTotal Item: **`)
+	.setDescription(`**Total Cash: ${userBalanceformat}💶\nTotal Item:${items} **`)
 	.setThumbnail(message.author.displayAvatarURL({ format: 'png' }))
 	.addFields(
 		{ name: 'Regular field title', value: 'Some value here' },
