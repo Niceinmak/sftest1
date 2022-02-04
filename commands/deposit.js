e2d90cf29936575858c6fe7567a5146be05cab0c
Glitch (phrygian-hospitable-plough) <none>
2022-02-04 17:09:15 +0000

    🚥🐪 Checkpoint ./commands/deposit.js:5606966/86 ./commands/bank.js:5606966/38

--- commands/deposit.js
+++ commands/deposit.js
@@ -4,7 +4,7 @@ exports.execute = async (client, message, args) => {
   if (!amount || isNaN(amount)) return message.channel.send(`Please enter a valid amount to transfer,${amount}`) 
   if(authordata.amount < amount) return message.channel.send('Looks like you don\'t have that much money') 
   client.eco.removeMoney(message.author.id, parseInt(amount));
-  client.eco.addMoney(`${message.author.id}_bank`, parseInt(amount));
+  client.eco.addMoney("333", parseInt(amount));
   let amountformat=String(amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   return message.channel.send(`You have successfully transferred 💶**${amountformat}** to ** ${message.author.tag}**.`)
 }
