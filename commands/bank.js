d669d42c28b21ee8c70f69ab370a391e9c078a15
Glitch (phrygian-hospitable-plough) <none>
2022-02-04 16:58:13 +0000

    🌥🍀 Checkpoint ./commands/deposit.js:5606966/371 ./commands/bank.js:5606966/881

---
+++ commands/bank.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = async (client, message, args) => {
+    let user = message.mentions.users.first() || message.author;
+    let userBalance = client.eco.fetchMoney(`${message.author.id}_bank`);
+    let userBalanceformat=String(userBalance.amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+    const embed = new MessageEmbed()
+        .setTitle(`Balance`)
+        .addField(`User`, `<@${userBalance.user}>`)
+        .addField(`Balance`, `**${userBalanceformat}**💶`)
+        .addField(`Position`, userBalance.position)
+        .setColor("RANDOM")
+        .setThumbnail(user.displayAvatarURL)
+        .setTimestamp();
+    return message.channel.send(embed);
+}
+
+exports.help = {
+    name: "bank",
+    aliases: ["BANK"],
+    usage: `bank`
+}
--- commands/deposit.js
+++ commands/deposit.js
@@ -1,12 +1,12 @@
 exports.execute = async (client, message, args) => {
-  let member = message.guild.members.cache.get(message.author) 
   let authordata = client.eco.fetchMoney(message.author.id) 
   let amount = args[0]
   if (!amount || isNaN(amount)) return message.channel.send(`Please enter a valid amount to transfer,${amount}`) 
   if(authordata.amount < amount) return message.channel.send('Looks like you don\'t have that much money') 
-  client.eco.addMoney(message.author.id, parseInt(amount));
+  client.eco.removeMoney(message.author.id, parseInt(amount));
+  client.eco.addMoney(`${message.author.id}_bank`, parseInt(amount));
   let amountformat=String(amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
-  return message.channel.send(`You have successfully transferred 💶**${amountformat}** to ** ${message.author}**.`)
+  return message.channel.send(`You have successfully transferred 💶**${amountformat}** to ** ${message.author.tag}**.`)
 }
 exports.help = {
   name: "deposit",
