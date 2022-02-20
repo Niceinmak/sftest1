bb049c8d15e3b60951632bcefa807c01bd18fd83
Glitch (phrygian-hospitable-plough) <none>
2022-02-20 11:19:05 +0000

    🎥🙎 Checkpoint ./commands/cf2.js:5606966/558

--- commands/cf2.js
+++ commands/cf2.js
@@ -29,9 +29,9 @@ namescardslistd=""
     let yazitura= ""
     let authordata = client.eco.fetchMoney(message.author.id) 
     let timecooldown = Math.floor(Math.random() * 200)+50;
-    let playtime = await client.eco.beg(client.ecoAddUser, timecooldown,{cooldown: 5000});
+    let playtime = await client.eco.work(client.ecoAddUser, timecooldown,{cooldown: 5000});
     const user1 = message.mentions.users.first() || message.member.user
-    if (playtime.onCooldown) return message.reply(`**Biraz yavaş ol,${playtime.time.seconds} saniye daha bekle.**`);
+    if (playtime.onCooldown) return message.reply(`**Take it slow,wait ${playtime.time.seconds} more seconds**`);
     let data2= client.eco.removeMoney(message.author.id, parseInt(timecooldown));
   //--------------------------------------------
   if(amount3=="all") amount3=authordata.amount;
@@ -42,7 +42,6 @@ namescardslistd=""
     else
     {
       if(amount3>50000)amount3=50000
-      let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
       let dealerd=`${drawCard("d")}`
       var argString = dealerd.substring(1).split(' ');
  // let argString = itemname.substr( itemname.indexOf(' ') + 1 );
@@ -217,6 +216,7 @@ function stopbj(){
     }
    else
       {
+        let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
              embed.setAuthor(`You Lose`)      
             let points=`${stopbj()}`
       var argString = points.substring(1).split(' ');
