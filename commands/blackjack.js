0aeb68104c01a62e088758c5e10f880652e534b5
Glitch (phrygian-hospitable-plough) <none>
2022-02-20 12:16:35 +0000

    ✋🎆 Checkpoint ./commands/blackjack.js:5606966/7149

--- commands/blackjack.js
+++ commands/blackjack.js
@@ -30,7 +30,7 @@ namescardslistd=""
     let authordata = client.eco.fetchMoney(message.author.id) 
     let timecooldown = Math.floor(Math.random() * 200)+50;
     let playtime = await client.eco.work(client.ecoAddUser, timecooldown,{cooldown: 5000});
-    const user1 = message.mentions.users.first() || message.member.user
+    const user1 = message.member.user
     if (playtime.onCooldown) return message.reply(`**Take it slow,wait ${playtime.time.seconds} more seconds**`);
     let data2= client.eco.removeMoney(message.author.id, parseInt(timecooldown));
   //--------------------------------------------
@@ -41,6 +41,7 @@ namescardslistd=""
     if(amount3>authordata.amount || amount3<1) return message.channel.send(`** ⛔${message.author.tag} | ** You don't have enough money`);
     else
     {
+      let messageid=message.author.id
       if(amount3>50000)amount3=50000
       let dealerd=`${drawCard("d")}`
       var argString = dealerd.substring(1).split(' ');
@@ -114,7 +115,7 @@ startbj()
         {
          embed.setAuthor(`You Win!`) 
           embed.setFooter(`You win ${amount3}`)
-          let data2= client.eco.addMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
         }
       else if(userpoint==dealerpoint)
         {
@@ -127,13 +128,13 @@ startbj()
             {
               embed.setAuthor(`You Win!`) 
               embed.setFooter(`You win ${amount3}`)
-          let data2= client.eco.addMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
             }
           else
             {
            embed.setAuthor(`You Lose`) 
               embed.setFooter(`You lose ${amount3}`)
-          let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.removeMoney(messageid, parseInt(amount3));
             }
         }
       return msg.edit(embed);
@@ -164,11 +165,11 @@ else
     { name: `Dealer \`${dpoint}\``, value: dcard, inline: true },
     { name: `${user1.username} \`${upoint}\``, value: ucard, inline: true },
 	)
-      if(userpoint>dealerpoint)
+     if(userpoint>dealerpoint)
         {
          embed.setAuthor(`You Win!`) 
           embed.setFooter(`You win ${amount3}`)
-          let data2= client.eco.addMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
         }
       else if(userpoint==dealerpoint)
         {
@@ -181,15 +182,16 @@ else
             {
               embed.setAuthor(`You Win!`) 
               embed.setFooter(`You win ${amount3}`)
-          let data2= client.eco.addMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
             }
           else
             {
            embed.setAuthor(`You Lose`) 
               embed.setFooter(`You lose ${amount3}`)
-          let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.removeMoney(messageid, parseInt(amount3));
             }
         }
+      console.log(messageid)
       return msg.edit(embed);
      
     }
@@ -228,11 +230,11 @@ else
     { name: `Dealer \`${dpoint}\``, value: dcard, inline: true },
     { name: `${user1.username} \`${upoint}\``, value: ucard, inline: true },
 	)
-      if(userpoint>dealerpoint)
+   if(userpoint>dealerpoint)
         {
          embed.setAuthor(`You Win!`) 
           embed.setFooter(`You win ${amount3}`)
-          let data2= client.eco.addMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
         }
       else if(userpoint==dealerpoint)
         {
@@ -245,13 +247,13 @@ else
             {
               embed.setAuthor(`You Win!`) 
               embed.setFooter(`You win ${amount3}`)
-          let data2= client.eco.addMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
             }
           else
             {
            embed.setAuthor(`You Lose`) 
               embed.setFooter(`You lose ${amount3}`)
-          let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
+          let data2= client.eco.removeMoney(messageid, parseInt(amount3));
             }
         }
       return msg.edit(embed);
@@ -291,7 +293,7 @@ function stopbj(){
     }
    else
       {
-        let data2= client.eco.removeMoney(message.author.id, parseInt(amount3));
+        let data2= client.eco.removeMoney(messageid, parseInt(amount3));
         embed.setFooter(`You lose ${amount3}`)
              embed.setAuthor(`You Lose`)      
             let points=`${stopbj()}`
