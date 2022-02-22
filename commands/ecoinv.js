da40db60c83ff598c5bb713e98b740c62ef5e22b
Glitch (phrygian-hospitable-plough) <none>
2022-02-22 08:35:36 +0000

    🕍🏯 Checkpoint ./commands/ecoinv.js:5606966/2327

--- commands/ecoinv.js
+++ commands/ecoinv.js
@@ -1,18 +1,22 @@
 const { MessageEmbed } = require("discord.js");
-
+const btcValue = require('btc-value');
 exports.execute = async (client, message, args) => {
+  btcValue().then(value => {
+    value=value.toString().slice(0,3);
     let user = message.mentions.users.first() || message.author;
     let userBalance = client.eco.fetchMoney(`${message.author.id}11`);
     let userBalanceformat=String(userBalance.amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+    let userBalanceformat2=String(userBalance.amount*value).replace(/(.)(?=(\d{3})+$)/g,'$1,')
     const embed = new MessageEmbed()
         .setTitle(`**EcoCoin🌿**`)
-        .setDescription(`**EcoCoin: \`${userBalanceformat}\`**`)
+        .setDescription(`**EcoCoin: \`${userBalanceformat}\`\nPrice: \`${userBalanceformat2}\`**`)
         .addField(`Usage`,`Buy \`${client.prefix} buyeco\` \nSell\`${client.prefix} selleco\``)
         .addField(`\`Note\` **This coin can only be used in-game.**`,`**Cannot be bought and sold in real life**`)
         .setColor("RANDOM")
         .setThumbnail(user.displayAvatarURL)
         .setTimestamp();
     return message.channel.send(embed);
+});
 }
 
 exports.help = {
