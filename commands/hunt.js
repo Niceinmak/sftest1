5ca7b451b411ea4deac936d2561b0ae6a4d2ca6c
Glitch (phrygian-hospitable-plough) <none>
2022-03-01 17:10:25 +0000

    🐴☃️ Checkpoint ./commands/buy.js:5606966/26 ./commands/hunt.js:5606966/15157 ./index.js:5606966/1468

---
+++ commands/hunt.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+exports.execute = async (client, message, args) => {
+  let animals = [
+    `You tripped while trying to rob ${target} and got caught!`,
+    `Getting sneaky eh? ${target} called the cops on you!`,
+    `You failed robbing ${target} becase you didn't subscribe to zerosync.`
+  ]
+  let userBalance = client.eco.fetchMoney(message.author.id);
+  if (userBalance.amount < 3) return message.channel.send("Looks like you are poor.");
+  let item = args[0];
+  if (!item) return message.channel.send("What are you trying to buy?");
+  let hasItem = client.shop[item];
+  if (!hasItem || hasItem == undefined) return message.reply("That item doesnt exists lol");
+  let isBalanceEnough = (userBalance.amount >= hasItem.cost);
+  if (!isBalanceEnough) return message.reply("Your balance is insufficient. You need :dollar: "+hasItem.cost+" to buy this item.");
+  let buy = client.eco.removeMoney(message.author.id, hasItem.cost);
+  
+  let itemStruct = {
+    name: item.toLowerCase(),
+    prize: hasItem.cost
+  };
+  
+  client.db.push(`items_${message.author.id}`, itemStruct);
+  return message.channel.send(`You purchased **${item}** for **:dollar: ${hasItem.cost}**.`);
+};
+
+exports.help = {
+  name: "hunt",
+  aliases: ["HUNT","h"],
+  usage: `hunt`
+};
