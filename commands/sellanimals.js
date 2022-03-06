ea9914e382519090fb83a8dc20ea42a6a26433f6
Glitch (phrygian-hospitable-plough) <none>
2022-03-06 10:28:38 +0000

    🏠🐯 Checkpoint ./commands/sellanimals.js:5606966/2220

--- commands/sellanimals.js
+++ commands/sellanimals.js
@@ -1,14 +1,56 @@
 const { MessageEmbed } = require("discord.js");
 
 exports.execute = async (client, message, args) => {
-  let users = [
-        "Pocket",
-        "T-Shirt",
-        "Zero's Databse",
-        "Street"
+  let userBalance = client.eco.fetchMoney(message.author.id);
+  let itemname=" "
+  let item = args[0];
+  let commonanimals = [
+        "<:god:948265037313757184>",
+        "<:cat1:948265025850724372>",
+        "<:fox:948265002492624976>",
+        "<:bison:948264912818429962>",
     ];
+  let uncommonanimals = [
+        "<:tiger:948264974856388639>",
+        "<:leopard:948264964597121146>",
+        "<:horse:948264952198746112>",
+    ];
+  let rareanimals = [
+        "<:deer:948264928387674213>",
+        "<:ox:948264893629480981>",
+        "<:pig:948264880417439804>",
+        "<:goat:948264872850886687>",
+    ];
+  let epicanimals = [
+        "<:llama:948264865552818196>",
+        "<:mouse:948264854551162910>",
+        "<:rabbit:948264845520801882>",
+        "<:koala:948264836322721862>",
+        "<:bear:948264822926094426>",
+    ];
+  let legendaryanimals = [
+        "<:bird:948264810980732988>",
+        "<:penguin:948264801698717728>",
+        "<:dodo:948264775639519232>",
+        "<:trex1:948264765866786907>",
+        "<:ant:948264757000040460>",
+    ];
+  const x = client.db.get(`animals_${message.author.id}`);
+    if (!x) {
+    return message.channel.send(`${message.author.tag} | Animals not found`);
+  }
+  const arrayToObject = x.reduce((itemStruct, x) => {
+    itemStruct[x.name] = (itemStruct[x.name] || 0) + 1;
+    return itemStruct;
+  }, {});
+  const result = Object.keys(arrayToObject).map(k =>
+       itemname+=k+" "+arrayToObject[k]+" "
+  //   message.channel.send(`**${k} Kasasını Sattın ve ${randomcash},${quantity*randomcash}💶 kazandın.${quantity}$,${count1},${itemname},,,${agr1},,,${agr2},,,${agr3}**`)
+  );
+  
     let amount = Math.floor(Math.random() * 200)+50;
     let amount3 = args[0]
+    console.log(itemname)
     const embed = new MessageEmbed()
         .setTitle(`Test`)
   message.channel.send(embed);
