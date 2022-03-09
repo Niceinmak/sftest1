b352c39e6272b3041dbe7d6bf30c4f282f07113a
Glitch (phrygian-hospitable-plough) <none>
2022-03-09 19:36:48 +0000

    🌝📈 Checkpoint ./commands/orangetext.js:5606966/6157 ./commands/kill.js:5606966/16379 ./commands/asciiart.js:5606966/652 ./commands/fakenitro.js:5606966/556

--- commands/fakenitro.js
+++ commands/fakenitro.js
@@ -1,6 +1,17 @@
 const { MessageEmbed } = require("discord.js");
-
+const discord = require("discord.js")
+const client = new discord.Client()
+const random = require("something-random-on-discord").Random
+let ms = require('ms')
+let db = require('quick.db');
 exports.execute = async (client, message, args) => {
+      const timeout = 10000;
+  const cooldown = await db.fetch(`cooldown_Command-Name_${message.author.id}`);
+      	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
+		const time = ms(timeout - (Date.now() - cooldown));
+		return message.reply(`**Wait ${time} to message again**`);
+	}
+  db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
  var links = [
       `https://discord.gift/Hejs82hejdi9`,
       `https://discord.gift/ejf88rjcUw8i`,
--- commands/orangetext.js
+++ commands/orangetext.js
@@ -1,38 +1,27 @@
 const { MessageEmbed } = require("discord.js");
-const figlet = require("figlet");
 const discord = require("discord.js")
 const client = new discord.Client()
 const random = require("something-random-on-discord").Random
 let ms = require('ms')
 let db = require('quick.db');
 exports.execute = async (client, message, args) => {
-    const timeout = 10000;
+        const timeout = 10000;
   const cooldown = await db.fetch(`cooldown_Command-Name_${message.author.id}`);
       	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
 		const time = ms(timeout - (Date.now() - cooldown));
 		return message.reply(`**Wait ${time} to message again**`);
 	}
   db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
-    let text = args.join(" ");
-   if (!text) {
-      return message.channel.send(`\`Usage:${client.prefix}ascii <msg>\``);
+    const text = args.join(" ");
+     if (!text) {
+      return message.channel.send("You need to enter some text");
     }
-   let maxlen = 20;
-    if (text.length > 20) {
-      return message.channel.send(
-        `Please put text that has 20 characters or less because the conversion won't be good!`
-      );
-    }
-   figlet(text, function (err, data) {
-      message.channel.send(data, {
-        code: "AsciiArt",
-      });
-    });
+    message.channel.send(`\`\`\`fix\n${text}\n\`\`\``);
   
 }
 
 exports.help = {
-    name: "asciiart",
-    aliases: ["ASCIIART"],
-    usage: `asciiart`
+    name: "orangetext",
+    aliases: ["ORANGETEXT"],
+    usage: `orangetext`
 }
