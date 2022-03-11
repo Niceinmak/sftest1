f6295a3569dee9986f1a57e6f98f8b0b6f72c702
Glitch (phrygian-hospitable-plough) <none>
2022-03-11 14:34:08 +0000

    🚟☃️ Checkpoint ./commands/leaderboard.js:5606966/98

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -3,22 +3,22 @@ const { MessageEmbed } = require("discord.js");
 exports.execute = async (client, message, args) => {
     let leaderboard = client.eco.leaderboard({ limit: 15, raw: false });
     if (!leaderboard || leaderboard.length < 1) return message.channel.send("❌ | Empty Leaderboard!");
-    const embed = new MessageEmbed()
+    //const embed = new MessageEmbed()
       //  .setAuthor(`${message.guild.name} ! Sunucusunun Liderler Sıralaması`, message.guild.iconURL)
-    .setAuthor(`EcoVerse Top 15 Leaderboard`, message.guild.iconURL)
-        .setColor("RANDOM")
-        .setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
-        .setTimestamp();
+    //.setAuthor(`EcoVerse Top 15 Leaderboard`, message.guild.iconURL)
+        //.setColor("RANDOM")
+        //.setThumbnail(client.users.cache.get(leaderboard[0].id) ? client.users.cache.get(leaderboard[0].id).displayAvatarURL : "https://cdn.discordapp.com/avatars/603948445362946084/a_f61398e073d78ae104e32b0517c891c3.gif")
+        //.setTimestamp();
   let leader="\`\`\`md\n| EcoVerse Top 15 Leaderboard |\n\nNote:Amounts of money here can be bank accounts or EcoCoins\n-------------------------------------------------------------\n"
     leaderboard.forEach(u => {
       let moneyformat=String(u.money).replace(/(.)(?=(\d{3})+$)/g,'$1,')
-        embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"}`, `**${moneyformat}** 💶`);
-      leader+=`<Position:${u.position} | ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"} ${moneyformat}💶 |>\n`
+       // embed.addField(`${u.position}. ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"}`, `**${moneyformat}** 💶`);
+      leader+=`<Position:${u.position} | ${client.users.cache.get(u.id) ? client.users.cache.get(u.id).tag : "Unknown#0000"}>\n<Money: ${moneyformat}💶>\n---------------------\n`
     });
   leader+="\n\`\`\`"
   message.channel.send("\`\`\`fix\nt\n\`\`\`"+"\`\`\`\nt\n\`\`\`");
-  message.channel.send(leader)
-    return message.channel.send(embed);
+  return message.channel.send(leader)
+    //return message.channel.send(embed);
 }
 
 exports.help = {
