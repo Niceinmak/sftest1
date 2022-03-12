aa5af24e008a401e2a496f5b01de2440d48ad974
Glitch (phrygian-hospitable-plough) <none>
2022-03-12 07:14:18 +0000

    🐾🍔 Checkpoint ./commands/advice.js:5606966/865

--- commands/advice.js
+++ commands/advice.js
@@ -1,5 +1,5 @@
 const discord = require("discord.js")
-const random = require("something-random-on-discord").Random
+const got = require('got');
 let ms = require('ms')
 let db = require('quick.db');
 exports.execute = async (client, message, args) => {
@@ -10,9 +10,27 @@ exports.execute = async (client, message, args) => {
 		return message.reply(`**Wait ${time} to message again**`);
 	}
   db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
-let data = await random.getMeme()
-console.log(data)
-   message.channel.send({ embed: data })
+const embed = new Discord.MessageEmbed();
+	got('https://www.reddit.com/r/memes/random/.json')
+		.then(response => {
+			const [list] = JSON.parse(response.body);
+			const [post] = list.data.children;
+
+			const permalink = post.data.permalink;
+			const memeUrl = `https://reddit.com${permalink}`;
+			const memeImage = post.data.url;
+			const memeTitle = post.data.title;
+			const memeUpvotes = post.data.ups;
+			const memeNumComments = post.data.num_comments;
+
+			embed.setTitle(`${memeTitle}`);
+			embed.setURL(`${memeUrl}`);
+			embed.setColor('RANDOM');
+			embed.setImage(memeImage);
+			embed.setFooter(`👍 ${memeUpvotes} 💬 ${memeNumComments}`);
+
+			message.channel.send(embed);
+		})
 }
 
 exports.help = {
