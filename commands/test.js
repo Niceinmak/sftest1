5097b0b98cf58b74e33cc8ed64bd3f499aafe294
Glitch (phrygian-hospitable-plough) <none>
2022-03-12 07:59:51 +0000

    🚗👸 Checkpoint ./commands/test.js:5606966/14306

--- commands/test.js
+++ commands/test.js
@@ -10,73 +10,41 @@ exports.execute = async (client, message, args) => {
     let amount = Math.floor(Math.random() * 200)+50;
     let amount3 = args[0]
     users.push("t")
-    const embed = new MessageEmbed()
-        .setTitle(`Test`)
-  message.channel.send({ embeds: [embed] });
-        msg.react("👍")
-  const filter = (reaction, user) => {
-	return reaction.emoji.name === '👍' && user.id === message.author.id;
-};
+       // Use a promise to wait for the question to reach Discord first
+        message.channel.send('Which emoji do you prefer?').then((question) => {
+          // Have our bot guide the user by reacting with the correct reactions
+          question.react('👍');
+          question.react('👎');
+    
+          // Set a filter to ONLY grab those reactions & discard the reactions from the bot
+          const filter = (reaction, user) => {
+            return ['👍', '👎'].includes(reaction.emoji.name) && !user.bot;
+          };
+    
+          // Create the collector
+          const collector = message.createReactionCollector({ filter, time: 15000 });
+          collector.on('end', (collected, reason) => {
+            if (reason === 'time') {
+              message.reply('Ran out of time ☹...');
+            } else {
+              // Grab the first reaction in the array
+              let userReaction = collected.array()[0];
+              // Grab the name of the reaction (which is the emoji itself)
+              let emoji = userReaction._emoji.name;
+    
+              // Handle accordingly
+              if (emoji === '👍') {
+                message.reply('Glad your reaction is 👍!');
+              } else if (emoji === '👎') {
+                message.reply('Sorry your reaction is 👎');
+              } else {
+                // This should be filtered out, but handle it just in case
+                message.reply(`I dont understand ${emoji}...`);
+              }
+            }
+          });
+        });
 
-const collector = message.createReactionCollector({ filter, time: 15000 });
-
-collector.on('collect', (reaction, user) => {
-	console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
-});
-
-collector.on('end', collected => {
-	console.log(`Collected ${collected.size} items`);
-});
-    /*
-    FARKLI KOMUTLAR DİZİNİ
-
-
-exports.execute = async (client, message, args) => {
-    let users = [
-        "Pocket",
-        "T-Shirt",
-        "Zero's Databse",
-        "Street"
-    ];
-    let amount = Math.floor(Math.random() * 200)+50;
-    let amount2 = Math.floor(Math.random() * 200)/100;
-    let amount3 = args[0]
-    let beg = await client.eco.beg(client.ecoAddUser, amount, { canLose: false, cooldown: 1, customName: "search" });
-    let beg2 = await client.eco.beg(client.ecoAddUser, amount2, { canLose: false, cooldown: 1, customName: "search" });
-    if (beg.onCooldown) return message.reply(`Come back after ${beg.time.minutes} minutes & ${beg.time.seconds} seconds.`);
-    return message.reply(`**${message.author.tag} | ** **${[Math.floor(beg2.amount)]}** Paranı **2'ye** **${amount}** katladın ve **${amount3}** 💸 Kazandın!. \n Şuanki Paran:**${beg.after}** 💸.`);
-       let data2= client.eco.removeMoney(message.author.id, parseInt(kullaniciveri));
-       let data2= client.eco.removeMoney(client.ecoAddUser, parseInt(kullaniciveri));    
-       let data2= client.eco.addMoney(message.author.id, parseInt(kullaniciveri));
-       let data2= client.eco.addMoney(client.ecoAddUser, parseInt(kullaniciveri));        
-       let data2= client.eco.setMoney(message.author.id, parseInt(kullaniciveri));
-        let data2= client.eco.setMoney(client.ecoAddUser, parseInt(kullaniciveri));           
----------------------------------------------------------------------------------------*/
-  
-  
-  /*module.exports.execute = async (client, message, args) => {
-      let jobs = [
-        "Doktor",
-        "Pompacı",
-        "Diş hekimi",
-        "Müzisyen",
-        "Yayıncı",
-        "Youtuber",
-        "Yazılımcı",
-        "Tesisatçı",
-        "Döşemeci"
-    ];
-    let amount = Math.floor(Math.random() * 1500) + 1000;
-    let work = client.eco.work(message.author.id, amount);
-    if (work.onCooldown) return message.reply(`You are tired rn. Come back after **${work.time.minutes} minutes & ${work.time.seconds} seconds** to work again.`);
-    else return message.reply(`You worked as **${jobs[Math.floor(Math.random() * jobs.length)]}** and earned **${work.amount}** 💶. Now you have **${work.after}** 💶.`);
-};
-
-module.exports.help = {
-    name: "work",
-    aliases: [],
-    usage: "work"
-} */
   
 }
 
