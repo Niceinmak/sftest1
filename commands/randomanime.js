1b2cfba52795c599bf31e15e4b98af10b1e74764
Glitch (phrygian-hospitable-plough) <none>
2022-03-12 07:25:33 +0000

    🌴🍶 Checkpoint ./commands/advice.js:5606966/12282 ./commands/randommeme.js:5606966/3570 ./commands/randomanime.js:5606966/714

--- commands/randomanime.js
+++ commands/randomanime.js
@@ -1,22 +1,19 @@
 const discord = require("discord.js")
-const random = require("something-random-on-discord").Random
+const randomanime = require('random-anime')
 let ms = require('ms')
 let db = require('quick.db');
 exports.execute = async (client, message, args) => {
   let userevent=args[0]
   const timeout = 10000;
-  if(userevent!="pat" && userevent!="hug" && userevent!="waifu" && userevent!="cry" && userevent!="kiss" && userevent!="slap" && userevent!="smug" && userevent!="punch")
-    {
-      return message.reply(`**Usage: \`${client.prefix}randomanime <category(pat/hug/waifu/cry/kiss/slap/smug/punch)>\`**`);
-    }
   const cooldown = await db.fetch(`cooldown_Command-Name_${message.author.id}`);
       	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
 		const time = ms(timeout - (Date.now() - cooldown));
 		return message.reply(`**Wait ${time} to message again**`);
 	}
-  db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
- let data = await random.getAnimeImgURL(userevent)
-    message.channel.send(data)
+  //db.set(`cooldown_Command-Name_${message.author.id}`, Date.now());
+const anime = randomanime.nsfw();
+    const embed = new discord.MessageEmbed().setImage(anime);
+   message.channel.send({ embeds: [embed] });
 }
 
 exports.help = {
