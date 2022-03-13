b788e45a6ee070eb03bfd0f1440bc79dd6045c3d
Glitch (phrygian-hospitable-plough) <none>
2022-03-13 17:15:36 +0000

    🕶🎤 Checkpoint ./commands/test.js:5606966/2721

--- commands/test.js
+++ commands/test.js
@@ -148,7 +148,99 @@ const collector = msg.createReactionCollector({ filter, time: 60000 });
         }          
          }
                 });
-
+                function stopbj(){
+  if(dealerpoint<17)
+    {
+      for(; ;)
+        {
+          drawCard("d")
+         if(dealerpoint>=17)
+           {
+             break
+           }
+        }
+      
+    }
+  return [` ${dealerpoint} ${userpoint} \`${namescardslistd}\` \`${namescardslistu}\``]
+}
+                  function startbj(){
+   let dealerd=`${drawCard("d")}`
+      var argString = dealerd.substring(1).split(' ');
+ // let argString = itemname.substr( itemname.indexOf(' ') + 1 );
+   //let agr1d=argString[0]
+  //let agr2d=argString[1]
+  let dealeru=`${drawCard("u")}`
+  var argString2 = dealeru.substring(1).split(' ');
+ // let argString = itemname.substr( itemname.indexOf(' ') + 1 );
+  let agr1u=argString2[0]
+  let agr2u=argString2[1]
+  if(userpoint<21)
+    {
+                embed.fields = [];
+  embed.addFields(
+    { name: `Dealer \`${agr1d}\``, value: agr2d, inline: true },
+    { name: `${user1.username} \`${agr1u}\``, value: agr2u, inline: true },
+	)
+    }
+     else
+    {
+         let points=`${stopbj()}`
+      var argString = points.substring(1).split(' ');
+ // let argString = itemname.substr( itemname.indexOf(' ') + 1 );
+  let dpoint=argString[0]
+  let upoint=argString[1]
+  let dcard=argString[2]
+  let ucard=argString[3]
+  embed.fields = [];
+  embed.addFields(
+    { name: `Dealer \`${dpoint}\``, value: dcard, inline: true },
+    { name: `${user1.username} \`${upoint}\``, value: ucard, inline: true },
+	)
+   if(userpoint>21 && dealerpoint>21)
+     {
+       embed.setFooter(`You both bust!`)
+          embed.setFooter(`You both bust!`)
+     }
+    else if(userpoint>21 && dealerpoint<=21)
+      {
+        embed.setAuthor(`You Lose`) 
+              embed.setFooter(`You lose ${amount3}`)
+          let data2= client.eco.removeMoney(messageid, parseInt(amount3));
+      }
+          else
+            {
+              if(userpoint>dealerpoint)
+        {
+         embed.setAuthor(`You Win!`) 
+          embed.setFooter(`You win ${amount3}`)
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
+        }
+      else if(userpoint==dealerpoint)
+        {
+          embed.setFooter(`You both bust!`)
+          embed.setFooter(`You both bust!`)
+        }
+      else
+        {
+          if(dealerpoint>21)
+            {
+              embed.setAuthor(`You Win!`) 
+              embed.setFooter(`You win ${amount3}`)
+          let data2= client.eco.addMoney(messageid, parseInt(amount3));
+            }
+          else
+            {
+           embed.setAuthor(`You Lose`) 
+              embed.setFooter(`You lose ${amount3}`)
+          let data2= client.eco.removeMoney(messageid, parseInt(amount3));
+            }
+        }
+            }
+     }
+ 
+    
+          return msg.edit(embed);
+}
       } );
     }
   }
