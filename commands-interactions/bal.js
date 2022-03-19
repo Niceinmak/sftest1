ca4075153e44d15e300db945e15f90ad0dca0280
Glitch (sftest) <none>
2022-03-19 12:08:35 +0000

    🎆🕵 Checkpoint ./index.js:5606966/8 ./commands-interactions/bal.js:5606966/1560

--- commands-interactions/bal.js
+++ commands-interactions/bal.js
@@ -1,9 +1,18 @@
 const { MessageEmbed,MessageButton,MessageActionRow } = require('discord.js');
 
 module.exports = {
-    description: 'Yardım Menüsü',
+    description: 'Show Your Balance',
+  options: [
+        {
+            name: 'user',
+            description: 'Select a user',
+            type: 'STRING',
+            required: false
+        },
+    ],
+  if(interaction.options.getChannel('user')==null)
     run: async (client, interaction) => {
-    let user = interaction.mentions.users.first() || interaction.author;
+    let user = interaction.mentions.first() || interaction.member.user.id;
     let userBalance = client.eco.fetchMoney(user.id);
     let userBalanceformat=String(userBalance.amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
     const embed = new MessageEmbed()
