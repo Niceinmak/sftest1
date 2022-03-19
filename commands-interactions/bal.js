000e38d06d6564396874c6688a5e7efe57809a3b
Glitch (sftest) <none>
2022-03-19 12:20:36 +0000

    ⛹🍜 Checkpoint ./commands-interactions/bal.js:5606966/1193

--- commands-interactions/bal.js
+++ commands-interactions/bal.js
@@ -6,22 +6,31 @@ module.exports = {
         {
             name: 'user',
             description: 'Select a user',
-            type: 'STRING',
+            type: 'USER',
             required: false
         },
     ],
-  if(interaction.options.getChannel('user')==null)
     run: async (client, interaction) => {
-    let user = interaction.mentions.first() || interaction.member.user.id;
-    let userBalance = client.eco.fetchMoney(user.id);
+      let userid=""
+      let args_user=interaction.options.getUser('user')
+        if(!args_user)
+  {
+    userid = args_user.id;
+  }
+  else
+  {
+    userid = interaction.member.user.id;
+  }      
+       console.log(userid, interaction.member.user.id)
+    let userBalance = client.eco.fetchMoney(userid);
     let userBalanceformat=String(userBalance.amount).replace(/(.)(?=(\d{3})+$)/g,'$1,')
     const embed = new MessageEmbed()
         .setTitle(`Balance`)
         .addField(`User`, `<@${userBalance.user}>`)
         .addField(`Balance`, `**${userBalanceformat}**💶`)
-        .addField(`Position`, userBalance.position)
+        .addField(`Position`, `${userBalance.position}`)
         .setColor("RANDOM")
-        .setThumbnail(user.displayAvatarURL)
+        .setThumbnail(userid.displayAvatarURL)
         .setTimestamp();
        interaction.reply({
            embeds:[embed],
