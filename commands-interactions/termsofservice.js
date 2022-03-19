bd3786484084906a980df2c062996d92b75170a9
Glitch (sftest) <none>
2022-03-19 16:45:37 +0000

    🐞🎊 Checkpoint ./commands-interactions/termsofservice.js:5606966/662

---
+++ commands-interactions/termsofservice.js
@@ -0,0 +1,17 @@
+const { MessageEmbed,MessageButton,MessageActionRow } = require('discord.js');
+
+module.exports = {
+    description: "EcoVerse's terms of use",
+    run: async (client, interaction) => {
+        const embed = new MessageEmbed()
+        .setTitle('Test')
+        .setDescription(`Test Succesfuly`)
+        .setThumbnail(client.user.avatarURL());
+        const buton = new MessageButton().setLabel('EcoVerse Website').setStyle('PRIMARY').setURL('http://ecoverse.ml');
+        const row = new MessageActionRow().addComponents(buton)
+       interaction.reply({
+           embeds:[embed],
+           components:[row],
+       })
+    }
+};
