b09ad1485d041135a8c03a4c924d72d51de5d0a2
Glitch (sftest) <none>
2022-03-20 17:46:35 +0000

    🚍🍅 Checkpoint ./commands-interactions/beg.js:5606966/643

---
+++ commands-interactions/beg.js
@@ -0,0 +1,17 @@
+const { MessageEmbed,MessageButton,MessageActionRow } = require('discord.js');
+
+module.exports = {
+    description: 'Dont have money, try begging!',
+    run: async (client, interaction) => {
+        const embed = new MessageEmbed()
+        .setTitle('Test')
+        .setDescription(`Test Succesfuly`)
+        .setThumbnail(client.user.avatarURL());
+        const buton = new MessageButton().setLabel('EcoVerse Website').setStyle('LINK').setURL('http://ecoverse.ml');
+        const row = new MessageActionRow().addComponents(buton)
+       interaction.reply({
+           embeds:[embed],
+           components:[row],
+       })
+    }
+};
