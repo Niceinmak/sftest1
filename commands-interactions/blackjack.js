063167162988185e6f9374ecf64d8d626401532e
Glitch (sftest) <none>
2022-03-22 16:47:36 +0000

    📒🙌 Checkpoint ./commands-interactions/blackjack.js:5606966/2505

--- commands-interactions/blackjack.js
+++ commands-interactions/blackjack.js
@@ -86,6 +86,16 @@ namescardslistd=""
             message.react("👊")
         //  msg.react("")  
           message.react("🛑")
+      const filter = message => message.content.includes('discord');
+const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
+
+collector.on('collect', m => {
+	console.log(`Collected ${m.content}`);
+});
+
+collector.on('end', collected => {
+	console.log(`Collected ${collected.size} items`);
+});
     }
     
     } 
