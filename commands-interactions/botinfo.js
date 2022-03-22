2e45aff7d019ddaca91de99881dc63efd910e1a4
Glitch (sftest) <none>
2022-03-22 14:49:30 +0000

    🌈👹 Checkpoint ./commands-interactions/botinfo.js:5606966/70

--- commands-interactions/botinfo.js
+++ commands-interactions/botinfo.js
@@ -11,8 +11,7 @@ module.exports = {
         },
     ],
     run: async (client, interaction) => {
-       let id=interaction.options.getString('text')
-       console.log(interaction.options.getString('text'))
+       let id=interaction.options.getString('guild_id')
     if (!client.cfg.admins.includes(interaction.user.id)) return interaction.reply("**Only bot administrators.**"); // return if author isn't bot owner
   let serverformat=String(client.guilds.cache.size).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let userformat=String(client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)).replace(/(.)(?=(\d{3})+$)/g,'$1,')
