50140e99db8c3e0f0e32464962bad4986ca1c013
Glitch (sftest) <none>
2022-03-24 14:41:52 +0000

    🎗🐠 Checkpoint ./commands-interactions/battle.js:5606966/3861

--- commands-interactions/battle.js
+++ commands-interactions/battle.js
@@ -13,24 +13,110 @@ module.exports = {
     run: async (client, interaction) => {
       await interaction.deferReply();
 		await wait(10);
+      let commonanimals = [
+        "<:god:948265037313757184>",
+        "<:cat1:948265025850724372>",
+        "<:fox:948265002492624976>",
+        "<:bison:948264912818429962>",
+    ];
+  let uncommonanimals = [
+        "<:tiger:948264974856388639>",
+        "<:leopard:948264964597121146>",
+        "<:horse:948264952198746112>",
+    ];
+  let rareanimals = [
+        "<:deer:948264928387674213>",
+        "<:ox:948264893629480981>",
+        "<:pig:948264880417439804>",
+        "<:goat:948264872850886687>",
+    ];
+  let epicanimals = [
+        "<:llama:948264865552818196>",
+        "<:mouse:948264854551162910>",
+        "<:rabbit:948264845520801882>",
+        "<:koala:948264836322721862>",
+        "<:bear:948264822926094426>",
+    ];
+  let legendaryanimals = [
+        "<:bird:948264810980732988>",
+        "<:penguin:948264801698717728>",
+        "<:dodo:948264775639519232>",
+        "<:trex1:948264765866786907>",
+        "<:ant:948264757000040460>",
+    ];
   let x = client.db.get(`teamanimals_${interaction.user.id}`);
       let animalcount=0
+      let args_user=interaction.options.getUser('user')
       const arrayToObject1 = x.reduce((itemStruct, x1) => {
     itemStruct[x.name] = (itemStruct[x.name] || 0) + 1;
     animalcount++
     return itemStruct;
   }, {});
       if(animalcount!=3) interaction.editReply(`**You must have 3 animals in the team to be able to battle.**`)
-      console.log(animalcount)
+      if(!args_user)
+        {
+      console.log(animalcount,args_user)
+          let name=""
+ const arrayToObject = x.reduce((itemsobj, x1) => {
+    itemsobj[x1.name] = (itemsobj[x1.name] || 0) + 1;
+    return itemsobj;
+  }, {});
+          console.log(name)
+  var argString = name.substring(1).split(' ');
+          const embed = new MessageEmbed()
+  .setTitle(`${interaction.user.username}'s Team`)
+  .setDescription(`**For all animals: \`/zoo\`**`)
+  for(let l=0;l<7;l+=3)
+    {
+      for(let i=0;i<commonanimals.length;i++)
+    {
+      console.log(argString[l])
+      if(argString[l]==commonanimals[i])
+        {
+          embed.addField(`Rarities: <:common:949006743428542545>`, `Name: ${argString[l]} Quantity:\` ${argString[l+1]}\` `, false)
+        }
+    }
+  for(let i=0;i<uncommonanimals.length;i++)
+    {
+      if(argString[l]==uncommonanimals[i])
+        {
+          embed.addField(`Rarities: <:uncommon:949006765696098345>`, `Name: ${argString[l]} Quantity:\` ${argString[l+1]}\` `, false)
+        }
+    }
+  for(let i=0;i<rareanimals.length;i++)
+    {
+      if(argString[l]==rareanimals[i])
+        {
+         embed.addField(`Rarities: <:rare:949006777519837225>`, `Name: ${argString[l]} Quantity:\` ${argString[l+1]}\` `, false)
+        }
+    }
+  for(let i=0;i<epicanimals.length;i++)
+    {
+      if(argString[l]==epicanimals[i])
+        {
+         embed.addField(`Rarities: <:epic:949006791201652827>`, `Name: ${argString[l]} Quantity:\` ${argString[l+1]}\` `, false)
+        }
+    }
+  for(let i=0;i<legendaryanimals.length;i++)
+    {
+      if(argString[l]==legendaryanimals[i])
+        {
+         embed.addField(`Rarities: <:legendary:949006805646864404>`, `Name: ${argString[l]} Quantity:\` ${argString[l+1]}\` `, false)
+        }
+    }
+    }
+  
+  return interaction.editReply({embeds:[embed]});
+        }
+      else
+        {
+          
+        }
         const embed = new MessageEmbed()
         .setTitle('Test')
         .setDescription(`Test Succesfuly`)
         .setThumbnail(client.user.avatarURL());
         const buton = new MessageButton().setLabel('EcoVerse Website').setStyle('LINK').setURL('http://ecoverse.ml');
         const row = new MessageActionRow().addComponents(buton)
-       interaction.editReply({
-           embeds:[embed],
-           components:[row],
-       })
     }
 };
