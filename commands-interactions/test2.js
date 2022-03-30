6e23c444e9794a94530883865d23527639036c47
Glitch (sftest) <none>
2022-03-30 16:50:22 +0000

    🌧💳 Checkpoint ./commands-interactions/test2.js:5606966/16392

--- commands-interactions/test2.js
+++ commands-interactions/test2.js
@@ -21,21 +21,17 @@ module.exports = {
 	// Return the result to use in the actual canvas
 	return context.font;
 };
+      
       const canvas = Canvas.createCanvas(700, 250);
 		const context = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('https://i.imgur.com/SpcEOfc.jpg');
+const background = await Canvas.loadImage('https://i.imgur.com/SpcEOfc.jpg');
 
 	// This uses the canvas dimensions to stretch the image onto the entire canvas
 	context.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	// Use the helpful Attachment class structure to process the file for you
-context.strokeRect(0, 0, canvas.width, canvas.height);
-const avatar = await Canvas.loadImage(interaction.user.displayAvatarURL({ format: 'jpg' }));
 context.strokeRect(0, 0, canvas.width, canvas.height);
 
-	// Pick up the pen
-	context.beginPath();
+	const avatar = await Canvas.loadImage(interaction.user.displayAvatarURL({ format: 'jpg' }));
+context.beginPath();
 
 	// Start the arc to form a circle
 	context.arc(125, 125, 100, 0, Math.PI * 2, true);
@@ -45,20 +41,19 @@ context.strokeRect(0, 0, canvas.width, canvas.height);
 
 	// Clip off the region you drew on
 	context.clip();
-	// Move the image downwards vertically and constrain its height to 200, so that it's square
+      
+	// Draw a shape onto the main canvas
 	context.drawImage(avatar, 25, 25, 200, 200);
-      
-	// Slightly smaller text placed above the member's display name
-	context.font = '28px sans-serif';
-	context.fillStyle = '#ffffff';
-	context.fillText('Profile', canvas.width / 2.5, canvas.height / 3.5);
+      context.font = '60px sans-serif';
 
-	// Add an exclamation point here and below
-	context.font = applyText(canvas, `${interaction.member.displayName}!`);
+	// Select the style that will be used to fill the text in
 	context.fillStyle = '#ffffff';
-	context.fillText(`${interaction.member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-      
+
+	// Actually fill the text with a solid color
+	context.fillText(interaction.member.displayName, canvas.width / 2.5, canvas.height / 1.8);
+	// Use the helpful Attachment class structure to process the file for you
 	const attachment = new MessageAttachment(canvas.toBuffer(), 'profile-image.png');
+
 	interaction.editReply({ files: [attachment] });
       
     }
