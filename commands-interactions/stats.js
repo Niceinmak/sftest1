4efb86c5bba4b7e68806dee673dc488f9aa34982
Glitch (sftest) <none>
2022-04-16 19:17:36 +0000

    🚌🐏 Checkpoint ./commands-interactions/stats.js:5606966/1108

--- commands-interactions/stats.js
+++ commands-interactions/stats.js
@@ -3,16 +3,20 @@ const { MessageEmbed,MessageButton,MessageActionRow } = require('discord.js');
 module.exports = {
     description: 'Shows bot statistics.',
     run: async (client, interaction) => {
+      const packageJSON = require("./package.json");
   let serverformat=String(client.guilds.cache.size).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let userformat=String(client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+  let latency=Math.abs(Date.now() - interaction.createdTimestamp)
+  let apilatency=Math.round(client.ws.ping)
+  let discordJSVersion = packageJSON.dependencies["discord.js"];
 const embed = new MessageEmbed()
 .addFields(
 		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: Math.abs(Date.now() - interaction.createdTimestamp), inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
+		{ name: '🌠 Latency', value: `${latency}`, inline: true },
+		{ name: '⭐ Api Latency', value: `${apilatency}`, inline: true },
+		{ name: '🌀 Discord.js Version', value: `${discordJSVersion}`, inline: true },
+		{ name: '💠 Total User', value: `${userformat}`, inline: true },
+		{ name: '🔰 Total Guild', value: `${serverformat}`, inline: true },
 		{ name: 'Inline field title', value: 'Some value here', inline: true },
 	)
 let servernames=""
