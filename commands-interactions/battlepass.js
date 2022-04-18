1a03a6a42b30bf56c39a455639d76c8e72f56867
Glitch (sftest) <none>
2022-04-18 17:11:57 +0000

    💁🤗 Checkpoint ./commands-interactions/battlepass.js:5606966/432

--- commands-interactions/battlepass.js
+++ commands-interactions/battlepass.js
@@ -5,16 +5,20 @@ module.exports = {
     run: async (client, interaction) => {
       await interaction.deferReply();
 		await wait(10);
+      let levels = []
+      for(let i=0;i<=20;i++)
+        {
+          levels.push(i)
+        }
+      console.log(levels)
         const embed = new MessageEmbed()
         	.setColor('#0099ff')
 	.setTitle('EcoVerse Battle Pass')
-        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}🎫***`)
+        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}<:ticket:965658088885583892>***`)
 	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
 	.addFields(
-		{ name: 'Level 1 [10]🎫', value: 'Some value here' },
+		{ name: 'Level 1 [10]<:ticket:965658088885583892>', value: `**Prize**;\n***10,000:euro: Cash***` },
 	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/AfFp7pu.png')
 	.setTimestamp()
 	.setFooter({ text: `${interaction.user.username}`});
        interaction.editReply({
