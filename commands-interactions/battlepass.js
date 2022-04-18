ff2c83657dd5d774fa4d27ff2b973bc5bc5152e3
Glitch (sftest) <none>
2022-04-18 16:59:59 +0000

    🍞💾 Checkpoint ./commands-interactions/battlepass.js:5606966/3520

--- commands-interactions/battlepass.js
+++ commands-interactions/battlepass.js
@@ -6,9 +6,17 @@ module.exports = {
       await interaction.deferReply();
 		await wait(10);
         const embed = new MessageEmbed()
-        .setTitle('Test')
-        .setDescription(`${client.db.get(`battlepass_${interaction.user.id}.point`)}`)
-        .setThumbnail(client.user.avatarURL());
+        	.setColor('#0099ff')
+	.setTitle('EcoVerse Battle Pass')
+        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}🎫***`)
+	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
+	.addFields(
+		{ name: 'Level 1 [10]🎫', value: 'Some value here' },
+	)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/AfFp7pu.png')
+	.setTimestamp()
+	.setFooter({ text: `${interaction.user.username}`});
        interaction.editReply({
            embeds:[embed],
        })
