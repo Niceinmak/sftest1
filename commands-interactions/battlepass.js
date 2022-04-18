37e5f7e390bf07fe9b6519858b24923a18a7ba07
Glitch (sftest) <none>
2022-04-18 17:59:56 +0000

    🏩📕 Checkpoint ./commands-interactions/battlepass.js:5606966/3508

--- commands-interactions/battlepass.js
+++ commands-interactions/battlepass.js
@@ -1,10 +1,22 @@
 const { MessageEmbed,MessageButton,MessageActionRow } = require('discord.js');
+const moment = require("moment");
+let ms = require('ms');
+let db = require('quick.db');
 const wait = require('node:timers/promises').setTimeout;
 module.exports = {
     description: 'battlepass command',
     run: async (client, interaction) => {
       await interaction.deferReply();
 		await wait(10);
+         //--------------------------------------------------------------
+            const timeout = 10000;
+  const cooldown = await db.fetch(`cooldown_openeggs_${interaction.user.id}`);
+      	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
+		const time = ms(timeout - (Date.now() - cooldown));
+          return interaction.editReply(`**Wait ${time} to message again**`)
+	}
+    db.set(`cooldown_openeggs_${interaction.user.id}`, Date.now());
+      //---------------------------------------------------------------------------
       let levels = []
       for(let i=0;i<=20;i++)
         {
@@ -21,8 +33,26 @@ module.exports = {
 	)
 	.setTimestamp()
 	.setFooter({ text: `${interaction.user.username}`});
+      const buton = new MessageButton().setCustomId('primary1').setEmoji('<:rightarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(false);
+      const buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:leftarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
+        const row = new MessageActionRow().addComponents(buton).addComponents(buton2)
        interaction.editReply({
            embeds:[embed],
+           components:[row],
        })
+      function selectLevel(level){
+        let returnlevel=""
+        for(let i=0;i<levels.lenght;i++)
+          {
+            if(i==level)
+              {
+                returnlevel+=`\`${levels[i]}\` `
+              }
+            else
+              {
+          returnlevel+=`**${levels[i]}** ` 
+              }
+          }
+      }
     }
 };
