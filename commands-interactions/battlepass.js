2e3a2b0b5b2d447171c4423a5d0ffae66250e1f2
Glitch (sftest) <none>
2022-04-19 15:13:29 +0000

    🐢🎖 Checkpoint ./commands-interactions/battlepass.js:5606966/6043

--- commands-interactions/battlepass.js
+++ commands-interactions/battlepass.js
@@ -22,7 +22,8 @@ module.exports = {
         {
           levels.push(i)
         }
-      let mainlevel=selectLevel(1)
+      let levelcount=1
+      let mainlevel=selectLevel(levelcount)
         const embed = new MessageEmbed()
         	.setColor('#0099ff')
 	.setTitle('EcoVerse Battle Pass')
@@ -33,13 +34,67 @@ module.exports = {
 	)
 	.setTimestamp()
 	.setFooter({ text: `${interaction.user.username}`});
-      const buton = new MessageButton().setCustomId('primary1').setEmoji('<:rightarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(false);
-      const buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:leftarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
+      const buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(true);
+      const buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
         const row = new MessageActionRow().addComponents(buton).addComponents(buton2)
        interaction.editReply({
            embeds:[embed],
            components:[row],
        })
+         const filter = i => i.user.id === interaction.user.id;
+
+const collector = interaction.channel.createMessageComponentCollector({time: 120000 });
+
+collector.on('collect', async i => {
+	if (i.customId === 'primary1') {
+    levelcount--
+      let mainlevel=selectLevel(levelcount)
+		i.deferUpdate();
+    embed.fields = [];
+    embed.setColor('#0099ff')
+	.setTitle('EcoVerse Battle Pass')
+        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}<:ticket:965658088885583892>***`)
+	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
+	.addFields(
+		{ name: 'Level 1 [10]<:ticket:965658088885583892>', value: `**Prize**;\n***10,000:euro: Cash***\n${mainlevel}` },
+	)
+	.setTimestamp()
+	.setFooter({ text: `${interaction.user.username}`});
+    let buton;
+    if(levelcount==1) buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(true); 
+    else buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(false);
+      const buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
+        const row = new MessageActionRow().addComponents(buton).addComponents(buton2)
+    return interaction.editReply({
+           embeds:[embed],
+           components:[row],
+       })
+	}
+	if (i.customId === 'primary2') {
+    levelcount++
+      let mainlevel=selectLevel(levelcount)
+		i.deferUpdate();
+    embed.fields = [];
+    embed.setColor('#0099ff')
+	.setTitle('EcoVerse Battle Pass')
+        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}<:ticket:965658088885583892>***`)
+	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
+	.addFields(
+		{ name: 'Level 1 [10]<:ticket:965658088885583892>', value: `**Prize**;\n***10,000:euro: Cash***\n${mainlevel}` },
+	)
+	.setTimestamp()
+	.setFooter({ text: `${interaction.user.username}`});
+      const buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(false);
+    let buton2;
+    if(levelcount==20) buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(true);
+    else buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
+        const row = new MessageActionRow().addComponents(buton).addComponents(buton2)
+    return interaction.editReply({
+           embeds:[embed],
+           components:[row],
+       })
+	}
+});
       function selectLevel(level){
         let returnlevel="\n"
         for(let i=1;i<levels.length;i++)
