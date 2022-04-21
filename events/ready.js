b1ca55ff4bc269e6dc0d523155d0573e21ed5292
Glitch (sftest) <none>
2022-04-21 16:15:24 +0000

    🎗💄 Checkpoint ./events/ready.js:5606966/76

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ module.exports = (client) => {
   let userformat=String(client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let server = client.guilds.cache.get("925628280785231872")
   let role = server.roles.cache.find(r => r.id === "964559669219700756");
+  role=role.members.map(m=>m.user.tag);
   console.log(role)
   client.user.setActivity(`/help | Bot not working? Please add again using the button above!`);
 };
