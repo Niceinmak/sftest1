598036d5a77c909a68475530bea62892ba259170
Glitch (sftest) <none>
2022-04-21 15:51:23 +0000

    🚠💄 Checkpoint ./events/ready.js:5606966/379

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,8 @@ module.exports = (client) => {
     console.log(`${client.user.tag} is online!`);
   let serverformat=String(client.guilds.cache.size).replace(/(.)(?=(\d{3})+$)/g,'$1,')
   let userformat=String(client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c)).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+  let server = client.guilds.cache.get("925628280785231872")
+  let role = server.roles.cache.find(r => r.id === "964559669219700756");
+  console.log(role)
   client.user.setActivity(`/help | Bot not working? Please add again using the button above!`);
 };
