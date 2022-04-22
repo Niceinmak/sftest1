a4fbe5c68d1f72eb6b9c7b83950d7e19f685150a
Glitch (sftest) <none>
2022-04-22 16:19:07 +0000

    🎅🎊 Checkpoint ./commands-interactions/battlepass.js:5606966/36 ./commands-interactions/claim.js:5606966/13347

--- commands-interactions/claim.js
+++ commands-interactions/claim.js
@@ -10,12 +10,12 @@ module.exports = {
 		await wait(10);
          //--------------------------------------------------------------
             const timeout = 10000;
-  const cooldown = await db.fetch(`cooldown_battlepass_${interaction.user.id}`);
+  const cooldown = await db.fetch(`cooldown_claim_${interaction.user.id}`);
       	if (cooldown !== null && timeout - (Date.now() - cooldown) > 0) {
 		const time = ms(timeout - (Date.now() - cooldown));
           return interaction.editReply(`**Wait ${time} to message again**`)
 	}
-    db.set(`cooldown_battlepass_${interaction.user.id}`, Date.now());
+    db.set(`cooldown_claim_${interaction.user.id}`, Date.now());
       //---------------------------------------------------------------------------
       let levels = []
       let prizes=[30,50,100,150,200,250,350,500,650,800,1000,1200,1450,1600,1900,2200,2500,3000,3500,4000]
@@ -25,10 +25,16 @@ module.exports = {
         }
       let levelcount=1
       let mainlevel=selectLevel(levelcount)
+      var prize = new Boolean(false)
+      let ticket=client.db.get(`battlepass_${interaction.user.id}.point`)
+      for(let i=0;i<=prizes.length;i++)
+        {
+        for(let j=0;j<=)
+        }
         const embed = new MessageEmbed()
         	.setColor('#0099ff')
 	.setTitle('EcoVerse Battle Pass')
-        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}<:ticket:965658088885583892>***`)
+        .setDescription(`***Your point:<:ticket:965658088885583892>***`)
 	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
 	.addFields(
 		{ name: `Level ${levelcount} [${prizes[levelcount-1]}]<:ticket:965658088885583892>`, value: `**Prize**;\n***10,000:euro: Cash***\n${mainlevel}` },
@@ -42,62 +48,7 @@ module.exports = {
            embeds:[embed],
            components:[row],
        })
-         const filter = i => i.user.id === interaction.user.id;
 
-const collector = interaction.channel.createMessageComponentCollector({time: 120000 });
-
-collector.on('collect', async i => {
-	if (i.customId === 'primary1') {
-    levelcount--
-      let mainlevel=selectLevel(levelcount)
-      let value=levelName(levelcount)
-		i.deferUpdate();
-    embed.fields = [];
-    embed.setColor('#0099ff')
-	.setTitle('EcoVerse Battle Pass')
-        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}<:ticket:965658088885583892>***`)
-	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
-	.addFields(
-		{ name: `Level ${levelcount} [${prizes[levelcount-1]}]<:ticket:965658088885583892>`, value: `${value}\n${mainlevel}` },
-	)
-	.setTimestamp()
-	.setFooter({ text: `${interaction.user.username}`});
-    let buton;
-    if(levelcount==1) buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(true); 
-    else buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(false);
-      const buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
-        const row = new MessageActionRow().addComponents(buton).addComponents(buton2)
-    return interaction.editReply({
-           embeds:[embed],
-           components:[row],
-       })
-	}
-	if (i.customId === 'primary2') {
-    levelcount++
-      let mainlevel=selectLevel(levelcount)
-      let value=levelName(levelcount)
-		i.deferUpdate();
-    embed.fields = [];
-    embed.setColor('#0099ff')
-	.setTitle('EcoVerse Battle Pass')
-        .setDescription(`***Your point:${client.db.get(`battlepass_${interaction.user.id}.point`)}<:ticket:965658088885583892>***`)
-	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
-	.addFields(
-		{ name: `Level ${levelcount} [${prizes[levelcount-1]}]<:ticket:965658088885583892>`, value: `${value}\n${mainlevel}` },
-	)
-	.setTimestamp()
-	.setFooter({ text: `${interaction.user.username}`});
-      const buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(false);
-    let buton2;
-    if(levelcount==20) buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(true);
-    else buton2 = new MessageButton().setCustomId('primary2').setEmoji('<:rightarrow:965666012911521841>').setStyle('PRIMARY').setDisabled(false);
-        const row = new MessageActionRow().addComponents(buton).addComponents(buton2)
-    return interaction.editReply({
-           embeds:[embed],
-           components:[row],
-       })
-	}
-});
       function selectLevel(level){
         let returnlevel="\n"
         for(let i=1;i<levels.length;i++)
