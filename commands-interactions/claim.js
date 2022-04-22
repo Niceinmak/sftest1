6b82fa0d0d4bfbbac71421fc41ddfc3188afbcf0
Glitch (sftest) <none>
2022-04-22 17:04:43 +0000

    🏎📉 Checkpoint ./commands-interactions/claim.js:5606966/1632

--- commands-interactions/claim.js
+++ commands-interactions/claim.js
@@ -24,7 +24,6 @@ module.exports = {
           levels.push(i)
         }
       let levelcount=1
-      let mainlevel=selectLevel(levelcount)
       var prize = new Boolean(false)
       let ticket=client.db.get(`battlepass_${interaction.user.id}.point`)
       let point=client.db.get(`battlepass_${interaction.user.id}.lastpoint`)
@@ -33,7 +32,7 @@ module.exports = {
       //client.db.set(`battlepass_${interaction.user.id}`, { point: point })
       for(let i=point;i<=ticket;i++)
         {
-        for(let j=0;j<=prizes.length;j++)
+        for(let j=0;j<prizes.length;j++)
           {
             console.log(i,prizes[j])
             if(i==prizes[j])
@@ -42,14 +41,12 @@ module.exports = {
               }
           }
         }
+      console.log(levelName(1))
         const embed = new MessageEmbed()
         	.setColor('#0099ff')
 	.setTitle('EcoVerse Battle Pass')
         .setDescription(`***Your point:<:ticket:965658088885583892>***`)
 	.setThumbnail(interaction.user.displayAvatarURL({ format: 'png' }))
-	.addFields(
-		{ name: `Level ${levelcount} [${prizes[levelcount-1]}]<:ticket:965658088885583892>`, value: `**Prize**;\n***10,000:euro: Cash***\n${mainlevel}` },
-	)
 	.setTimestamp()
 	.setFooter({ text: `${interaction.user.username}`});
       const buton = new MessageButton().setCustomId('primary1').setEmoji('<:leftarrow:965666012794069003>').setStyle('PRIMARY').setDisabled(true);
@@ -60,48 +57,31 @@ module.exports = {
            components:[row],
        })
 
-      function selectLevel(level){
-        let returnlevel="\n"
-        for(let i=1;i<levels.length;i++)
-          {
-            if(i==11)
-              {
-                returnlevel+=`\n`
-              }
-            if(i==level)
-              {
-                returnlevel+=`\`${levels[i]}\` `
-              }
-            else
-              {
-          returnlevel+=`**${levels[i]}** ` 
-              }
-          }
-        return returnlevel
-      }
             function levelName(level){
-        let returnlevel="" 
-       if(level==1) returnlevel=`**Prize**;\n***1,000:euro: Cash***`
-      else if(level==2) returnlevel=`**Prize**;\n***5,000:euro: Cash***`
-      else if(level==3) returnlevel=`**Prize**;\n***1 Common Case***`
-      else if(level==4) returnlevel=`**Prize**;\n***10 Rare Egg***`
-      else if(level==5) returnlevel=`**Prize**;\n***25,000:euro: Cash***`
-      else if(level==6) returnlevel=`**Prize**;\n***30,000:euro: Cash***`
-      else if(level==7) returnlevel=`**Prize**;\n***5 Common Case***`
-      else if(level==8) returnlevel=`**Prize**;\n***5 Epic Egg***`
-      else if(level==9) returnlevel=`**Prize**;\n***1 Rare Case***`
-      else if(level==10) returnlevel=`**Prize**;\n***120,000:euro: Cash***`
-      else if(level==11) returnlevel=`**Prize**;\n***1 Rare Cash + 2 Common Case***`
-      else if(level==12) returnlevel=`**Prize**;\n***400 EcoCoin***`
-      else if(level==13) returnlevel=`**Prize**;\n***500 EcoCoin***`
-      else if(level==14) returnlevel=`**Prize**;\n***220,000:euro: Cash***`
-      else if(level==15) returnlevel=`**Prize**;\n***3 Rare Case***`
-      else if(level==16) returnlevel=`**Prize**;\n***450,000:euro: Cash***`
-      else if(level==17) returnlevel=`**Prize**;\n***20 Epic Egg***`
-      else if(level==18) returnlevel=`**Prize**;\n***650,000:euro: Cash***`
-      else if(level==19) returnlevel=`**Prize**;\n***1000 EcoCoin***`
-      else if(level==20) returnlevel=`**Prize**;\n***100,000,000:euro: Cash***`
-        return returnlevel
+        let returnleveltext="" 
+        let t=""
+        let commonitem="common.case"
+       if(level==1) client.eco.addMoney(interaction.user.id, parseInt(1000)),returnleveltext=`**Prize**;\n***1,000:euro: Cash\n***`
+      else if(level==2) returnleveltext=client.eco.addMoney(interaction.user.id, parseInt(5000)),`**Prize**;\n***5,000:euro: Cash\n***`
+      else if(level==3) let hasItem = client.shop[item];,returnleveltext=`**Prize**;\n***1 Common Case\n***`
+      else if(level==4) returnleveltext=`**Prize**;\n***10 Rare Egg\n***`
+      else if(level==5) returnleveltext=`**Prize**;\n***25,000:euro: Cash\n***`
+      else if(level==6) returnleveltext=`**Prize**;\n***30,000:euro: Cash\n***`
+      else if(level==7) returnleveltext=`**Prize**;\n***5 Common Case\n***`
+      else if(level==8) returnleveltext=`**Prize**;\n***5 Epic Egg\n***`
+      else if(level==9) returnleveltext=`**Prize**;\n***1 Rare Case\n***`
+      else if(level==10) returnleveltext=`**Prize**;\n***120,000:euro: Cash\n***`
+      else if(level==11) returnleveltext=`**Prize**;\n***1 Rare Cash + 2 Common Case\n***`
+      else if(level==12) returnleveltext=`**Prize**;\n***400 EcoCoin\n***`
+      else if(level==13) returnleveltext=`**Prize**;\n***500 EcoCoin\n***`
+      else if(level==14) returnleveltext=`**Prize**;\n***220,000:euro: Cash\n***`
+      else if(level==15) returnleveltext=`**Prize**;\n***3 Rare Case\n***`
+      else if(level==16) returnleveltext=`**Prize**;\n***450,000:euro: Cash\n***`
+      else if(level==17) returnleveltext=`**Prize**;\n***20 Epic Egg\n***`
+      else if(level==18) returnleveltext=`**Prize**;\n***650,000:euro: Cash\n***`
+      else if(level==19) returnleveltext=`**Prize**;\n***1000 EcoCoin\n***`
+      else if(level==20) returnleveltext=`**Prize**;\n***100,000,000:euro: Cash\n***`
+      return returnleveltext
       }
     }
 };
